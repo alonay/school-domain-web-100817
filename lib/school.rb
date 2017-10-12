3fcdde8b4826b584f82f6cb620de4bda72c3e9bd
github username <github email address>
2017-10-11 20:51:21 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,38 @@
-# code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  def name=(name)
+    @name=name
+  end
+  def name
+    @name
+  end
+  def roster
+    @roster
+  end
+  def add_student(student_name, grade)
+    #@roster[grade] = []
+    #@roster[grade] << student_name
+    if @roster.key?(grade)
+      @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade] << student_name
+    end
+  end
+  def grade(num)
+    @roster[num]
+  end
+  def sort
+    @sorted_roster = {}
+
+    @roster.map do |grade, names|
+      @sorted_roster[grade] = names.sort
+    end
+
+    return @sorted_roster
+  end
+
+end
